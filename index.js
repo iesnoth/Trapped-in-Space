437a9883a62cb592403c1f57951f58c499e66879
Victoria Beasley <iesnoth91@gmail.com>
2022-09-26 18:59:48 -0500

    redid background as gifs to condense layers

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-move(newButton('assets/switch-tl/off.png')).to(350, 820)
+const myAssets = {
+    bottomConsole: "assets/bott-console.gif"
 
+}
+
+
+move(newButton('assets/switch-tl/off.png')).to(350, 820)
+move(myAssets.bottomConsole).to(0,0)
 
 //makes a new button
 function newButton(url){
     let button = document.createElement('img')
     button.src = url
-    button.style.position = 'absolute';
+    button.style.position = 'relative';
     button.setAttribute = ('class','switch');
     button.setAttribute = ('value','off');
     document.getElementById("top").appendChild(button);
@@ -23,7 +29,7 @@ function buttonFunction(url){
         newButton(url)
         }
         else{
-            
+
         }
     })
     return item
@@ -42,3 +48,21 @@ function move(element) {
         to:moveTo
     }
 }
+
+// function backgroundAnimation()
+//         {
+//             requestAnimationFrame(backgroundAnimation);
+
+//             let url = document.getElementById('bg').src;
+
+//             if (url == "assets/bg1.png")
+//             {
+//                 document.getElementById('bg').src = "assets/bg2.png";
+//             }
+
+//             else if (url == "assets/bg2.png")
+//                 {
+//                     document.getElementById('bg').src = "assets/bg1.png";
+//                 }
+//         }
+// backgroundAnimation()
