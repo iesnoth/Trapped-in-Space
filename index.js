3ed570220666f74b9fa552b11bfb62cb0aa9363a
Victoria Beasley <iesnoth91@gmail.com>
2022-09-27 19:38:35 -0500

    reduced amount of buttons lol

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const buttonArray = [
-//Top Console
+    //Top Console
     [
         //Left panel
         //Top Row
-        move(newButton('assets/switch-tl/off.png', 'left-buttons')).to(19,-30),
-        move(newButton('assets/switch-tl/off.png', 'left-buttons')).to(23,-19),
-        move(newButton('assets/switch-tl/off.png', 'left-buttons')).to(29,-8),
+        move(newButton('assets/switch-tl/off.png', 'left-buttons')).to(19, -30),
+        move(newButton('assets/switch-tl/off.png', 'left-buttons')).to(23, -19),
+        move(newButton('assets/switch-tl/off.png', 'left-buttons')).to(29, -8),
         //Bottom Row
-        move(newButton('assets/switch-tl/off.png', 'left-buttons')).to(-44,-74),
-        move(newButton('assets/switch-tl/off.png', 'left-buttons')).to(-40,-64),
-        move(newButton('assets/switch-tl/off.png', 'left-buttons')).to(39,-6),
+        move(newButton('assets/switch-tl/off.png', 'left-buttons')).to(-44, -74),
+        move(newButton('assets/switch-tl/off.png', 'left-buttons')).to(-40, -64),
+        move(newButton('assets/switch-tl/off.png', 'left-buttons')).to(39, -6),
         //Circle TV
         move(newButton('assets/circle-tv/off.png', 'top')).to(-124, 603),
         move(newButton('assets/circle-tv/off.png', 'top')).to(-62, 606),
@@ -25,20 +25,26 @@ const buttonArray = [
         move(newButton('assets/center-switch/off.png', 'center-buttons')).to(-45, -54),
         move(newButton('assets/center-switch/off.png', 'center-buttons')).to(40, -18),
         //Right panel
-        move(newButton('assets/circle-tr/off.png', 'right-buttons')).to(19,-18),
-        move(newButton('assets/circle-tr/off.png', 'right-buttons')).to(30,-29),
-        move(newButton('assets/circle-tr/off.png', 'right-buttons')).to(-33,-49),
-        move(newButton('assets/circle-tr/off.png', 'right-buttons')).to(-23,-60)
+        move(newButton('assets/circle-tr/off.png', 'right-buttons')).to(19, -18),
+        move(newButton('assets/circle-tr/off.png', 'right-buttons')).to(30, -29),
+        move(newButton('assets/circle-tr/off.png', 'right-buttons')).to(-33, -49),
+        move(newButton('assets/circle-tr/off.png', 'right-buttons')).to(-23, -60)
     ],
     //Bottom Panel
+    []
+]
+
+const switchArray = [
     [
-        
+        //Center throttle
+        move(newButton('assets/center-dial/dial.png', 'center-console')).to(-69,-55),
+        move(newButton('assets/center-dial/dial.png', 'center-console')).to(69,-55),
+        //center dials
+
 
     ]
 
 ]
-
-const switchArray = []
 
 //makes a new button
 function newButton(url, id) {
