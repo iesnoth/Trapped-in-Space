f5e1fd567414060308d811d16197eb5ab28cb039
Victoria Beasley <iesnoth91@gmail.com>
2023-03-10 23:24:03 -0600

    trying to fix doom screen

--- index.js
+++ index.js
@@ -102,4 +102,8 @@ const buttonArray = [
 
 ]
 
+//border buttons
+move(buttonFunction('assets/border-buttons/restart.png', 'assets/border-buttons/restart-dark.png', 'border-div', 'restart')).to(380, 73)
+move(buttonFunction('assets/border-buttons/info.png', 'assets/border-buttons/infodark.png', 'border-div', 'info')).to(382, 73)
+
 
